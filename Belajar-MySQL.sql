32d3ff78241249a0a81d6389baf9b7b5ad22e0bf
Vidky Adhi Pradana <vickypradana1111@gmail.com>
2024-10-13 14:23:43 +0700

    Belajar IF, case, date

--- Belajar-MySQL.sql
+++ Belajar-MySQL.sql
@@ -8,7 +8,7 @@ SELECT * FROM matkul;
 
 SHOW TABLES;
 
-DESC skor_mhs;
+DESCRIBE skor_mhs;
 
 SHOW CREATE TABLE skor_mhs;
 
@@ -24,12 +24,16 @@ ALTER TABLE matkul
 	ADD COLUMN kelamin ENUM("L","P")
 		AFTER lecturer;
 
-UPDATE matkul
-SET kelamin = "L"
-WHERE subject_name = "Mattek";
+UPDATE skor_mhs
+SET gender = "L"
+WHERE name = "Jojon";
 
-INSERT INTO skor_mhs (score_id, student_id, name, score, subject_id)
-VALUES (7, 4, "Andre", 89, 11);
+UPDATE skor_mhs
+SET grade = 0
+WHERE name = "Jojon";
+
+INSERT INTO skor_mhs (student_id, name,gender, score, subject_id)
+VALUES  ( 13, "Haria","P", 67, 12);
 
 SELECT * FROM skor_mhs WHERE score > 80;
 
@@ -46,8 +50,8 @@ SET kelamin = "L"
 WHERE name = "Andre";
 
 DELETE 
-FROM matkul
-WHERE subject_id = 15;
+FROM skor_mhs
+WHERE score_id = 11;
 
 SELECT 	s.waktu_dibuat	AS Timestap,
 		s.score_id 		AS "ID Skor",
@@ -79,6 +83,71 @@ WHERE grade IS NULL;
 SELECT * 
     FROM skor_mhs
     WHERE score NOT BETWEEN 50 AND 80;
+
+ALTER TABLE skor_mhs
+    RENAME COLUMN waktu_dibuat TO timestap,
+    RENAME COLUMN kelamin TO gender;
+    
+SELECT * 
+	FROM skor_mhs
+    ORDER BY score ASC;
+    
+SELECT * 
+FROM skor_mhs
+WHERE score > 50
+ORDER BY score DESC
+LIMIT 3, 2;
+
+SELECT distinct gender FROM skor_mhs;
+
+ALTER TABLE matkul
+MODIFY subject_id INT NOT NULL AUTO_INCREMENT;
+
+SELECT name, score * 10 as 'ratusan'
+FROM skor_mhs
+WHERE score * 10 > 500
+ORDER BY ratusan ASC 
+LIMIT 2,2;
+
+SELECT score_id,name,
+		UPPER(name) as "Upper Name",
+		LENGTH(name) as "Length of Name"
+FROM skor_mhs;
+
+SELECT  timestap, score_id, name,
+    EXTRACT(MONTH FROM timestap) as Bulan,
+    EXTRACT(DAY FROM timestap) as Tanggal
+FROM skor_mhs;
+
+SELECT score_id, name, MONTH(timestap), DAY(timestap) 
+FROM skor_mhs;
+
+SELECT score_id, name,
+		score,
+        IF(score <= 50, "Mendho",
+        IF(score <= 70, "Goblok", "Pinter")) AS "Utek e"
+FROM skor_mhs;
+
+SELECT name, 
+		CASE gender
+			WHEN "L" THEN "Kakung"
+            WHEN "P" THEN "Putri"
+            END AS "Jawa pride"
+FROM skor_mhs;
+
+UPDATE skor_mhs
+SET score = 20
+WHERE name = "Ani";
+
+SELECT name, score,
+		IF(score < 50, "Wedok an goblok", 
+        IF (score < 80, "B aja", "Cewe Pinter")) AS "Utek"
+FROM skor_mhs
+WHERE gender = "P";
+
+SELECT score_id, name, IFNULL(grade, '0')
+FROM skor_mhs;
+    
 
         
 
