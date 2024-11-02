fe9d1246a1abc96aa37a69bf1b420796cbd63e39
Vidky Adhi Pradana <vickypradana1111@gmail.com>
2024-11-02 10:57:12 +0700

    Belajar Locking table

--- user1.sql
+++ user1.sql
@@ -1,5 +1,5 @@
 START TRANSACTION;
-
+ 
 SELECT * FROM guestbooks;
 
 UPDATE guestbooks 
@@ -9,8 +9,36 @@ WHERE id IN(5);
 COMMIT;
 
 -- manual locking
-SELECT * FROM skor_mhs WHERE score_id = 7 FOR UPDATE;
+SELECT * FROM skor_mhs WHERE score_id = 9 FOR UPDATE;
+SELECT * FROM skor_mhs WHERE score_id = 8 FOR UPDATE;
 
 UPDATE skor_mhs
-SET score = score - 12
-WHERE score_id = 7;
+SET score = score + 5
+WHERE score_id = 9;
+
+-- LOCK TABLE READ
+LOCK TABLE skor_mhs READ;
+
+
+SELECT * FROM skor_mhs;
+
+-- LOCK TABLE WRITE
+LOCK TABLE skor_mhs WRITE;
+
+UPDATE skor_mhs
+SET grade = 2
+WHERE score_id = 10;
+
+UNLOCK TABLE;
+
+-- locking instance 
+LOCK INSTANCE FOR BACKUP;
+
+-- tidak bisa (lock instance)
+ALTER TABLE skor_mhs
+ADD COLUMN grade VARCHAR(100) AFTER subject_id;
+
+
+
+
+ 
